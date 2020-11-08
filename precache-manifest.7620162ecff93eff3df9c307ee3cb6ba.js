ec45d65f3009653a5d22b1aa8a2d2f8f7898db48
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-08 17:47:20 +0100

    Updates

---
+++ precache-manifest.7620162ecff93eff3df9c307ee3cb6ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "babf037584b66cc2d936",
+    "url": "/ToDo-List/static/css/main.f003418f.chunk.css"
+  },
+  {
+    "revision": "babf037584b66cc2d936",
+    "url": "/ToDo-List/static/js/main.38c30215.chunk.js"
+  },
+  {
+    "revision": "aa01cae781a8458028e2",
+    "url": "/ToDo-List/static/js/runtime~main.d8d2c0bc.js"
+  },
+  {
+    "revision": "d8ea4e540511c59ad1e5",
+    "url": "/ToDo-List/static/js/2.5442d947.chunk.js"
+  },
+  {
+    "revision": "3b74538b62e0b1e5c1fdf2e1e932fd2c",
+    "url": "/ToDo-List/index.html"
+  }
+];
